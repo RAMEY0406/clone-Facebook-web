cfb5e0900ee12ec472524f22a0fc35280aaec1b6
RA MEY <ramey0406@gmail.com>
2024-03-17 23:07:40 +0700

    dropdown

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// Get the element
+var dropdown_click = document.getElementById('dropdown_search');
+
+// Add event listener for click
+dropdown_click.addEventListener('click', function() {
+    // Set opacity to 1 to make it appear
+    dropdown_search.style.opacity = '1';
+});
+
+console.log(dropdown_click);
